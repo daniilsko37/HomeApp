8ba94c8f3c328e300a30c0bb5bb5e579c2dd7fd0
Даниил Скорницкий <dansko@Daniils-MacBook-Air.local>
2026-05-24 16:23:40 +0300

    Принудительная загрузка шрифтов через Font.loadAsync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,47 @@
+import * as Font from 'expo-font';
 import { Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import '../global.css';
+
 export default function RootLayout() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      try {
+        // Заставляем сам движок Expo скачать шрифты из интернета, минуя локальную сборку
+        await Font.loadAsync({
+          Ionicons: 'https://unpkg.com/@expo/vector-icons@14.0.2/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf',
+          Feather: 'https://unpkg.com/@expo/vector-icons@14.0.2/build/vendor/react-native-vector-icons/Fonts/Feather.ttf',
+          FontAwesome: 'https://unpkg.com/@expo/vector-icons@14.0.2/build/vendor/react-native-vector-icons/Fonts/FontAwesome.ttf',
+          MaterialIcons: 'https://unpkg.com/@expo/vector-icons@14.0.2/build/vendor/react-native-vector-icons/Fonts/MaterialIcons.ttf',
+        });
+        setFontsLoaded(true);
+      } catch (e) {
+        console.warn('Ошибка загрузки шрифтов:', e);
+        setFontsLoaded(true); 
+      }
+    }
+    
+    loadFonts();
+  }, []);
+
+  // Ждем загрузки шрифтов
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack 
         screenOptions={{ 
           headerShown: false,
-          // 🪄 ГЛОБАЛЬНАЯ МАГИЯ: включаем нативные свайпы для ВСЕХ экранов приложения
           gestureEnabled: true, 
-          // Задаем красивую нативную анимацию перехода по умолчанию
           animation: 'slide_from_right' 
         }}
       >
-        {/* Главный экран с табами (с него свайпать назад некуда) */}
         <Stack.Screen name="(tabs)" />
-        
-        {/* Экраны, которые открываются поверх табов и поддерживают свайп назад */}
         <Stack.Screen name="statistics" />
         <Stack.Screen name="shop" />
       </Stack>

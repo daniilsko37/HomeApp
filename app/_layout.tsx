4e823bdf92137ef38b113a70f23188b4dfc9606c
Даниил Скорницкий <dansko@Daniils-MacBook-Air.local>
2026-05-24 16:19:27 +0300

    CDN фикс для иконок

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,22 @@
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import '../global.css';
-
 export default function RootLayout() {
-  // Жестко привязываем пути к файлам шрифтов, чтобы веб-сборщик их не потерял
-  const [fontsLoaded] = useFonts({
-    Ionicons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf'),
-    Feather: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Feather.ttf'),
-    FontAwesome: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/FontAwesome.ttf'),
-    MaterialIcons: require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialIcons.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack 
         screenOptions={{ 
           headerShown: false,
+          // 🪄 ГЛОБАЛЬНАЯ МАГИЯ: включаем нативные свайпы для ВСЕХ экранов приложения
           gestureEnabled: true, 
+          // Задаем красивую нативную анимацию перехода по умолчанию
           animation: 'slide_from_right' 
         }}
       >
+        {/* Главный экран с табами (с него свайпать назад некуда) */}
         <Stack.Screen name="(tabs)" />
+        
+        {/* Экраны, которые открываются поверх табов и поддерживают свайп назад */}
         <Stack.Screen name="statistics" />
         <Stack.Screen name="shop" />
       </Stack>
